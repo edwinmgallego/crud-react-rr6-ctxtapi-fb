237ee52ca8c8521e2175b0e6723c5368022297d8
edwinmgallego <ing.edwin.gallego@gmail.com>
2024-10-31 18:44:16 -0500

    agregando  formulario refactorizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./routes/Login";
 import Home from "./routes/Home";
+import Perfil from "./routes/Perfil";
+import LayoutRedirect from "./Components/Layout/LayaoutRedirect";
+import NotFound from "./routes/NotFound";
+
 import Navbar from "./Components/navbar";
-import RequireAuth from "./Components/RequireAuth";
+import RequireAuth from "./Components/Layout/RequireAuth";
 import "./App.css";
-import Register from "./Components/Register";
+import Register from "./routes/Register";
 import { UserContext } from "./context/UserProvider";
-import LayoutContainerForm from "./Components/LayoutContainerForm";
+import LayoutContainerForm from "./Components/Layout/LayoutContainerForm";
 
 function App() {
   const { user } = useContext(UserContext);
@@ -22,17 +26,17 @@ function App() {
       <h1>APP</h1>
 
       <Routes>
-        <Route
-          path="/"
-          element={
-            <RequireAuth>
-              <Home />
-            </RequireAuth>
-          }
-        ></Route>
+        <Route path="/" element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/perfil" element={<Perfil />} />
+        </Route>
         <Route path={"/"} element={<LayoutContainerForm />}>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+        </Route>
+
+        <Route path="/:nanoid" element={<LayoutRedirect />}>
+          <Route index element={<NotFound />} />
         </Route>
       </Routes>
     </>
